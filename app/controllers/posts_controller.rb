5bcc0a9e55357c200a374ac9891454fc2566afe3
camilo9020 <camilo9020@gmail.com>
2016-07-02 11:07:56 -0500

    complete CRUD of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,58 @@
 class PostsController < ApplicationController
 
 	def index
+
+		@posts= Post.all.order('created_at ASC')
 		
+	end
+
+	def show
+		@post= Post.find(params[:id])		
+	end
+
+
+	def new
+		@post= Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			flash[:success]= "El post ha sido creado con éxito"
+			redirect_to posts_path
+		else
+			render :new
+		end		
+	end
+
+
+	def edit
+		@post = Post.find(params[:id])		
+	end
+
+
+	def update
+		post = Post.find(params[:id])
+		if post.update(post_params) 
+			flash[:success]= "El post fue editado con éxito"
+			redirect_to posts_path
+		else
+			render :edit
+		end
+
+	end
+
+	def destroy
+		Post.find(params[:id]).destroy
+		flash[:success]= "El post fue elimnado con éxito"
+		redirect_to posts_path		
+	end
+
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :body)
 		
 	end
 
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,7 @@
 module PostsHelper
+
+	def form_title
+		@post.new_record? ? "Crear Post" : "Editar Post"		
+	end
+
 end
